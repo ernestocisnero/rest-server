1f9d83a74b36701105fcb4cf2d68943c05f4d910
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2023-03-07 19:36:51 -0600

    Default port  added

--- models/Server.js
+++ models/Server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.userRoutesPath = '/api/user';
 
         //Middlewares
